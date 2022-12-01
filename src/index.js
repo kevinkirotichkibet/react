618112f87ae6b07416f3bef9c0a4101903a387f4
Kevin Kirotich Kibet <kirotichkevin@gmail.com>
2022-12-01 13:15:01 +0300

    initial react commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+
+const myFirstElement = <h1>This is react</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  myFirstElement
+);
+
+
