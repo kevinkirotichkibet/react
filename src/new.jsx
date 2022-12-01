a0541774c26e0ce83cb8dee59eb815ececc39b32
kevinkirotichkibet <83207553+kevinkirotichkibet@users.noreply.github.com>
2022-12-01 12:42:45 +0000

    new file added

---
+++ src/new.jsx
@@ -0,0 +1,18 @@
+const myElement = (
+
+    <table>
+        <tr>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <td>Kevin</td>
+        </tr>
+        <tr>
+            <td>Kirotich</td>
+        </tr>
+        <tr>
+            <td>Kibet</td>
+        </tr>
+    </table>
+
+)
